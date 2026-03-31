3cf9c389dc4fe8c883d30efc602d8ebc324ce4d5
jagseeun <s2468@e-mirim.hs.kr>
2026-03-31 09:57:59 +0900

    CounterApp+최대 10까지, if, 삼항연산자, Math.min()

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -10,12 +10,19 @@ function CounterApp() {
 
   return (
     <>
-     <h1>{count}</h1>
-     <button onClick={() => setCount((count) => count + 1)}>+</button>
-     <button onClick={() => setCount((count) => count + 2)}>+2</button>
-     <button onClick={() => setCount((count) => count - 1)}>-</button>
-     <button onClick={() => setCount(0)}>reset</button>
-     {/* <button onClick={setCount(0)}>reset2</button> Too many re-renders*/}
+      <h1>{count}</h1>
+      <button onClick={() => setCount((count) => count + 1)}>+</button>
+      <button onClick={() => setCount((count) => count + 2)}>+2</button>
+      <button onClick={() => setCount((count) => count - 1)}>-</button>
+      <button onClick={() => setCount(0)}>reset</button>
+      <button onClick={() => setCount((count) => count >= 10 ? count : count + 1)}>최대 10까지(3항 연산자)</button>
+      <button onClick={() => setCount((count) => Math.min(count+1,10))}>최대 10까지(Math.min)</button>
+      {/* <button onClick={setCount(0)}>reset2</button> Too many re-renders*/}
+      <button onClick={()=>setCount((count)=>{
+        if(count+1>=10) return 10;
+        return count + 1;
+      })}>최대 10까지(if)</button>
+      
       {/* <button
         className="counter"
         onClick={() => setCount((count) => count + 1)} //JS 함수 호출 X 함수 정의
