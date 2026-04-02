9631f65e2676107f4032e09a36d947d49b1e310f
jagseeun <s2468@e-mirim.hs.kr>
2026-04-02 14:27:58 +0900

    TodoList reset.css 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
+import './reset.css'
 import TodoListApp from '../TodoListApp'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
