6ed73d1c801a74464138bba08a99a67deecfbc56
jagseeun <s2468@e-mirim.hs.kr>
2026-04-07 09:35:48 +0900

    TodoListApp HTML todo__item--empty

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -3,12 +3,18 @@ import "./todolist.css"
 function TodoListApp() {
     return (
         <div class="todo">
+
+
             <h1 class="todo__title">Todo List App</h1>
             <form class="todo__form">
-                <input type="text" placeholder="할 일 목록을 입력하세요." className="todo__input"/>
+                <input type="text" placeholder="할 일 목록을 입력하세요." className="todo__input" />
                 <button type="submit" className="todo__button todo__button--add">추가</button>
             </form>
+
             <ul className="todo__list">
+                <li className="todo__item todo_item--empty">
+                    <p>할 일 없음</p>
+                </li>
                 <li className="todo__item todo__item--completed">
                     <input type="checkbox" className="todo__check" name="" id="chk-1" />
                     <label htmlFor="chk-1" className="todo__label"> 옷싸기</label>
@@ -20,4 +26,4 @@ function TodoListApp() {
     )
 }
 
-export default TodoListApp
+export default TodoListApp 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './reset.css'
-import TodoListApp from '../TodoListApp'
+import TodoListApp from './TodoListApp'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <TodoListApp />
