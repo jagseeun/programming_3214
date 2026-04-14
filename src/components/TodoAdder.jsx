926a475bce82827e95feaa9dfe0dde712fccba46
jagseeun <s2468@e-mirim.hs.kr>
2026-04-14 10:11:23 +0900

    TodoListApp addTodo()

--- src/components/TodoAdder.jsx
+++ src/components/TodoAdder.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import Button from "./Button.jsx" //import 추가 안했어서 오류 났었음!! 근데 폴더까지 써서 또 다른 ㅇ에러가,,ㅋ
 export default function TodoAdder({addTodo}) { //const {addTodo} = props; //props에서 addTodo만 꺼내서 사용하겠다. -> 구조분해할당\
-    const [inputTodo, setInputTodo] = useState('');
+    const [inputTodo, setInputTodo] = useState("");
     const handleSubmit=(event)=>{
         event.preventDefault(); //기본 동작 막기
+        if(!inputTodo.trim()) return; //input이 빈 문자열이거나 공백만 있는 경우 제출 막기
         addTodo(inputTodo); //input에 있는 text value를 할일 내용으로 추가
+        setInputTodo(""); //input 초기화
     }
     return (
         <form className="todo__form" onSubmit={handleSubmit}>
