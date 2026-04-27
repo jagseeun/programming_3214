5c9e9490b593ebdf2ca35ea1c19634c93e2921c9
jagseeun <s2468@e-mirim.hs.kr>
2026-04-28 08:35:57 +0900

    edit, delete 추가하기

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,21 +1,25 @@
+import { useState } from 'react'
 import "./todolist.css"
-import { useState } from "react"
-import TodoHeader from "./components/TodoHeader.jsx" //import 추가 안했어서 오류 났었음!!
-import TodoAdder from "./components/TodoAdder.jsx" 
+import Button from "./components/Button.jsx"
+import Checkbox from "./components/Checkbox.jsx"
+import TodoItemEmpty from "./components/TodoItemEmpty.jsx"
+import TodoHeader from "./components/TodoHeader.jsx"
+import TodoAdder from "./components/TodoAdder.jsx"
+import TodoItem from "./components/TodoItem.jsx"
 import TodoList from "./components/TodoList.jsx"
 
-class Todo{
-    constructor(id, text, isCompleted){
-        this.id=id;
-        this.text=text;
-        this.isCompleted=isCompleted; // true or false
+class Todo {
+    constructor(id, text, isCompleted) {
+        this.id = id;
+        this.text = text;
+        this.isCompleted = isCompleted; // true or false
     }
 }
 function TodoListApp() {
     const [todos, setTodos] = useState([]) // Todo 객체들의 배열 (기본값은 빈 배열)
-    function addTodo(text){
+    function addTodo(text) {
         //이전 todos에 newTodo 만들어서 추가하자 -> 그것을 setTodos() 하자
-        setTodos((todos)=>[
+        setTodos((todos) => [
             ...todos,  //dotos에 있는 item을 다 꺼내서 새로운 리스트에서 시작
             // todos, 
             new Todo(
@@ -25,35 +29,49 @@ function TodoListApp() {
             )
         ]);
     }
-    function toggleTodo(id){
+    function toggleTodo(id) {
         //let o1 = {a:10, b:20} b수정을 하고 싶음 o1.b=200 변함 깊은 복사 얕은 복사 이렇게 하면 안 먹음.
         //o1을 스프레드 연산자로 o2에 넣으면 새로 만들어지고 o1과 o2는 다른 객체가 됨 -> 얕은 복사
         //o1=o2할 시에 주소값을 공유해서 o1과 o2는 같은 객체가 됨 -> 깊은 복사 
-        setTodos((todos)=>
-            
+        setTodos((todos) =>
+
             //todos에서 하나식 꺼내서, todo, 꺼낸 todo의 id와 파라미터의 id가 같다면,
             //새 객체 만들어서 todo값 복사, 속성 수정
-            todos.map((todo)=>
-                todo.id===id?{...todo, isCompleted: !todo.isCompleted} : todo
+            todos.map((todo) =>
+                todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+            )
+        )
+    }
+    function deleteTodo(id) {
+        // todos를 하나씩 꺼내어 todo, todo.id === id
+        setTodos((todos) =>
+            todos.filter((todo) => todo.id !== id)
+        )
+    }
+    function editTodo(id, newText) {
+        //todos 하나씩 꺼내어 todo. id가 같으면 text: newText
+        setTodos((todos) =>
+            todos.map((todo) =>
+                todo.id === id ? { ...todo, text: newText } : todo
             )
         )
     }
     return (
         <div className="todo">
 
-            <TodoHeader/>
-            <TodoAdder addTodo={addTodo}/>
-            <TodoList todos={todos} toggleTodo={toggleTodo}/>
+            <TodoHeader />
+            <TodoAdder addTodo={addTodo} />
+            <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} editTodo={editTodo}/>
         </div>
     )
 }
 
-export default TodoListApp 
+export default TodoListApp
 //const [count, setCount] = usetState(0)
 //count : number
 // 값이 바뀐 -> 리액트 인식 -> 값 새로 업데이트
 
 //const [todos, settodos] = userState([])
-//todos : list 
+//todos : list
 //3학년 2반 리스트 위치로 값 변경함
 //->3학년 3반 교실 로 바꿔야 아 바뀌었구나! 함 교실에 한 명씩 옮기고 새로운 학생 추가 -> 그래야 react가 바뀌었다고 인식
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,17 +1,52 @@
-import Button from "./Button.jsx"
+import { useState } from 'react'
 import Checkbox from "./Checkbox.jsx"
-export default function TodoItem({todo, toggleTodo}){
-    return(
-        //todo.iscompleted가 treu면 "todo__item--complete" false ""
+import Button from "./Button.jsx"
+
+export default function TodoItem({ todo, toggleTodo, deleteTodo, editTodo }) {
+    const [isEditing, setIsEditing] = useState(false);  //수정중인지 아닌지
+    const [editText, setEditText] = useState(todo.text);    //수정중인 text
+    function handleEditClick() {
+        if (!isEditing) {   //edit 시작
+            setIsEditing(true);
+            setEditText(todo.text);
+        } else {            //edit 끝
+            const trimmedText = editText.trim();
+            //빈칸이 아니고, 이전 text가 아닐때만 editTodo()
+            if (trimmedText !== "" && trimmedText !== todo.text) {
+                editTodo(todo.id, trimmedText);
+            }
+            setIsEditing(false);
+        }
+    }
+    return (
+        // todo.isCompleted가 true 면 " todo__item--complete", false ""
         <li className={`todo__item${todo.isCompleted ? " todo__item--complete" : ""}`}>
-            <Checkbox 
-            id={todo.id} 
-            checked={todo.isCompleted}
-            onChange={()=>toggleTodo(todo.id)}
-            
-            >{todo.text}</Checkbox>
-            <Button className="todo__button todo__button--edit">✏️</Button>
-            <Button className="todo__button todo__button--delete">❌</Button>
+            {!isEditing &&
+                <Checkbox
+                    id={todo.id}
+                    checked={todo.isCompleted}
+                    onChange={() => toggleTodo(todo.id)}
+                >{todo.text}</Checkbox>
+            }
+            {isEditing &&
+                <input
+                    type="text"
+                    className="todo__input--edit"
+                    value={editText}
+                    onChange={(event) => setEditText(event.target.value)}
+                    //enter 치면, handleEditClick 호출하자
+                    onKeyDown={(event) => { if (event.key === "Enter") handleEditClick() }}
+                    autoFocus
+                />
+            }
+            <Button
+                className="todo__button todo__button--edit"
+                onClick={handleEditClick}
+            >{isEditing ? "💾" : "✏️"}</Button>
+            <Button
+                className="todo__button todo__button--delete"
+                onClick={() => deleteTodo(todo.id)}
+            >🗑️</Button>
         </li>
     )
 }
